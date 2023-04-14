1d53eb5dba2ca1831682b79eca03ac59b9c97d56
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-04-14 17:57:05 +0530

    Update builds.sh

--- builds.sh
+++ builds.sh
@@ -4,7 +4,11 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "******************************************************************"
-
+python3.9 manage.py createsuperuser
+admin
+admin
+admin
+y
 echo "Make Migration...1"
 python3.9 manage.py makemigrations --noinput
 echo "Make Migration...2"
