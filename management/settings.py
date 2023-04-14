63a5fe77d8109fa5f54f15658940a09d4253a055
kishan savaliya <kishansavaliya9083@gmail.com>
2023-04-14 16:58:06 +0530

    change for login page

--- management/settings.py
+++ management/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ak6dbt_ttg=^5!jghys(tbf4t(ii&jp&(7a*=(4fr35q6r45vv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = ["b831-2405-201-200c-823f-fd5d-e7d7-124e-66fb.in.ngrok.io"]
