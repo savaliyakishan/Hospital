487780bd2665b30a07a850b12dc4536242c609f3
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-04-16 12:48:14 +0530

    Update builds.sh

--- builds.sh
+++ builds.sh
@@ -14,16 +14,14 @@ python3.9 manage.py migrate --noinput
 
 echo "###################################################################"
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
-# python3 manage.py shell
-# from django.contrib.auth.models import User
-# user=User.objects.create_user('admin', password='admin')
-# user.is_superuser=True
-# user.is_staff=True
-# user.save()
-winpty python manage.py createsuperuser
-admin
-admin@g.com
-admin
+
+python manage.py shell
+from django.contrib.auth.models import User
+user=User.objects.create_user('admin', password='admin')
+user.is_superuser=True
+user.is_staff=True
+user.save()
+
 echo "#################################dfasfdsdsf##################################"
 
 python3.9 manage.py collectstatic --noinput
