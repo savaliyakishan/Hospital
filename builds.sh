311367d0cc213d7c43f4bf0e123cd9cfa322a988
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-04-16 12:50:37 +0530

    Update builds.sh

--- builds.sh
+++ builds.sh
@@ -15,12 +15,9 @@ python3.9 manage.py migrate --noinput
 echo "###################################################################"
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
 
-python manage.py shell
+python3 manage.py shell
 from django.contrib.auth.models import User
-user=User.objects.create_user('admin', password='admin')
-user.is_superuser=True
-user.is_staff=True
-user.save()
+User.objects.create('admin', password='admin',is_superuser="True",is_staff="True")
 
 echo "#################################dfasfdsdsf##################################"
 
