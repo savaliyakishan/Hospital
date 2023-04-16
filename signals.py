ed55aeb127cf305f25a3f89717be9cc4cfe54cd3
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-04-16 13:15:04 +0530

    Create signals.py

---
+++ signals.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+
+@receiver(post_migrate)
+def create_superuser(sender, **kwargs):
+    """Create a superuser when the application is first deployed."""
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'password')
