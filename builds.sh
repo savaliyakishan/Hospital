58cfb828036f00ba4400ecbca576f3f309a734ea
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-04-16 12:21:01 +0530

    Update builds.sh

--- builds.sh
+++ builds.sh
@@ -1,20 +1,26 @@
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+# python3.9 -m pip install -r requirements.txt
 
-echo "******************************************************************"
+# echo "******************************************************************"
 
-echo "Make Migration...1"
-python3.9 manage.py makemigrations --noinput
-echo "Make Migration...2"
-python3.9 manage.py makemigrations hospital --noinput
-echo "Make Migration...3"
-python3.9 manage.py migrate --noinput
+# echo "Make Migration...1"
+# python3.9 manage.py makemigrations --noinput
+# echo "Make Migration...2"
+# python3.9 manage.py makemigrations hospital --noinput
+# echo "Make Migration...3"
+# python3.9 manage.py migrate --noinput
 
 echo "###################################################################"
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
-python3.9 manage.py createsuperuser
+manage.py shell
+from django.contrib.auth.models import User
+user=User.objects.create_user('admin', password='admin')
+user.is_superuser=True
+user.is_staff=True
+user.save()
+
 echo "#################################dfasfdsdsf##################################"
 
 python3.9 manage.py collectstatic --noinput
