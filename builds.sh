2f51e3ac47755a074c56f844588b006905302abd
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-04-16 12:28:02 +0530

    Update builds.sh

--- builds.sh
+++ builds.sh
@@ -5,12 +5,12 @@ python3.9 -m pip install -r requirements.txt
 
 # echo "******************************************************************"
 
-# echo "Make Migration...1"
-# python3.9 manage.py makemigrations --noinput
-# echo "Make Migration...2"
-# python3.9 manage.py makemigrations hospital --noinput
-# echo "Make Migration...3"
-# python3.9 manage.py migrate --noinput
+echo "Make Migration...1"
+python3.9 manage.py makemigrations --noinput
+echo "Make Migration...2"
+python3.9 manage.py makemigrations hospital --noinput
+echo "Make Migration...3"
+python3.9 manage.py migrate --noinput
 
 echo "###################################################################"
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
