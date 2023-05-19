783d944d036f27f21fca1da180646d92c926089c
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-05-19 17:35:46 +0530

    Update settings.py

--- management/settings.py
+++ management/settings.py
@@ -77,16 +77,24 @@ WSGI_APPLICATION = 'management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': config('ENGINE'),
+#        'NAME': config('NAME'),
+#        'USER': config('USER'),
+#        'PASSWORD': config('PASSWORD'),
+#        'HOST': config('HOST'),
+#        'PORT': config('PORT'),
+#    }
+# }
+
 DATABASES = {
-   'default': {
-       'ENGINE': config('ENGINE'),
-       'NAME': config('NAME'),
-       'USER': config('USER'),
-       'PASSWORD': config('PASSWORD'),
-       'HOST': config('HOST'),
-       'PORT': config('PORT'),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
