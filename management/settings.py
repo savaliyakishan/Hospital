04afc9840fc0eac5fc6c2dbc631b2bc310ab44ac
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-05-23 17:46:35 +0530

    Update settings.py

--- management/settings.py
+++ management/settings.py
@@ -87,16 +87,6 @@ DATABASES = {
        'PORT': config('PORT'),
    }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -136,9 +126,10 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 if DEBUG:
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'hospital/static')]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'hospital/static')]
 else:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'hospital/static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'hospital/static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
