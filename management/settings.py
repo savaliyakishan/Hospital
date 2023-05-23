8fcb92db704b0514c1ff4b3c3a6085f798fb6874
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-05-23 17:56:49 +0530

    Update settings.py

--- management/settings.py
+++ management/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ak6dbt_ttg=^5!jghys(tbf4t(ii&jp&(7a*=(4fr35q6r45vv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh','*']
 # ALLOWED_HOSTS = ["b831-2405-201-200c-823f-fd5d-e7d7-124e-66fb.in.ngrok.io"]
@@ -125,10 +125,7 @@ AUTH_USER_MODEL = 'hospital.User'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'hospital/static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'hospital/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'hospital/static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
