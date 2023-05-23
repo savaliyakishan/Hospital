a92a08b579ede3a8edf799de771f5d274f88f2d3
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-05-23 17:36:39 +0530

    Update urls.py

--- management/urls.py
+++ management/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('',include('hospital.urls')),
     path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
