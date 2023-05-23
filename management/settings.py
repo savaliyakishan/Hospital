0721ce5a61bf0930583764040ac9bae209044c6d
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-05-23 17:49:02 +0530

    Update settings.py

--- management/settings.py
+++ management/settings.py
@@ -126,9 +126,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'hospital/static')]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, './static')]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'hospital/static')
+    STATIC_ROOT = os.path.join(BASE_DIR, './static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
