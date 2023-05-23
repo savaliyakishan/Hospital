31fc5d6296712f42ce141372f115ab6dff65a87c
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-05-23 17:37:43 +0530

    Update builds.sh

--- builds.sh
+++ builds.sh
@@ -14,4 +14,4 @@ python3.9 manage.py migrate --noinput
 
 echo "###################################################################"
 
-python3.9 manage.py collectstatic --noinput
+#python3.9 manage.py collectstatic --noinput
