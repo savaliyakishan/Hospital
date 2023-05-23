3bb05dcadad809d1895a76f17093176f0000a0e8
savaliyakishan <92784681+savaliyakishan@users.noreply.github.com>
2023-05-23 17:41:53 +0530

    Update builds.sh

--- builds.sh
+++ builds.sh
@@ -12,6 +12,6 @@ python3.9 manage.py makemigrations hospital --noinput
 echo "Make Migration...3"
 python3.9 manage.py migrate --noinput
 
-echo "###################################################################"
+echo "####################################collect###############################"
 
-#python3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
